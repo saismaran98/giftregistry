2139eb88ac9f249c8f0290259239e664c7ea71bc
Saismaran Hota <saismaranhota@Saismarans-MacBook-Air.local>
2024-04-30 23:08:17 +0530

    initial project setup commit

---
+++ src/main/java/com/app/giftregistry/GiftregistryApplication.java
@@ -0,0 +1,13 @@
+package com.app.giftregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftregistryApplication.class, args);
+	}
+
+}
