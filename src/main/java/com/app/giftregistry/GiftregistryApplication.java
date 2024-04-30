97b6591b6e649aefd8fd97669f9adde3c3f6f721
Saismaran Hota <saismaran.hota@ibm.com>
2024-04-30 22:20:43 +0530

    initial project commit

---
+++ src/main/java/com/app/giftregistry/GiftregistryApplication.java
@@ -0,0 +1,18 @@
+package com.app.giftregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftregistryApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GiftregistryApplication.class, args);
+	}:
+
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
+}
